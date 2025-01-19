e00d9380440721e953b5c23289fdd11ca849e3df
marcosuper97 <gorpishin97@yandex.ru>
2025-01-19 18:47:30 +0300

    Верстка UI

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -16,5 +16,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }
+
+    fun new (str: String){
+
     }
 }
