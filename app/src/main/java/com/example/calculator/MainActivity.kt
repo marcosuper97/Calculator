a77d0abb949c1d235d134b8081b8ea8c4e8eed31
marcosuper97 <gorpishin97@yandex.ru>
2025-01-20 12:58:54 +0300

    Обработка нажатий чисел и очистка значения

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,10 +20,37 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        var count: String = ""
+
+        fun appendNumber(number: String) {
+            if (count.length < 10) { // Ограничение длины
+                count += number
+                binding.count.text = count
+            } else {
+                Toast.makeText(this, "Maximum length reached", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        fun cleanCount(){
+            count = ""
+            binding.count.text = count
+        }
+
+        binding.one.setOnClickListener() { appendNumber("1") }
+        binding.two.setOnClickListener() { appendNumber("2") }
+        binding.three.setOnClickListener() { appendNumber("3") }
+        binding.four.setOnClickListener() { appendNumber("4") }
+        binding.five.setOnClickListener() { appendNumber("5") }
+        binding.six.setOnClickListener() { appendNumber("6") }
+        binding.seven.setOnClickListener() { appendNumber("7") }
+        binding.eight.setOnClickListener() { appendNumber("8") }
+        binding.nine.setOnClickListener() { appendNumber("9") }
+        binding.zero.setOnClickListener() { appendNumber("0") }
+        binding.point.setOnClickListener() { appendNumber(".") }
+        binding.ac.setOnClickListener(){cleanCount()}
     }
 
-    fun new (str: String){
-
-    }
 }
