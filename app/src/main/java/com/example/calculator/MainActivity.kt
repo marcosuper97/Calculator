53e7d908955e6b9468821e43733336f57b24501f
marcosuper97 <gorpishin97@yandex.ru>
2025-01-30 21:16:58 +0300

    30.01.25

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.HorizontalScrollView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    lateinit var countToTokenizer: String
+    private val handler = Handler(Looper.getMainLooper())
+    lateinit var count: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +33,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val spaceController = SpaceController()
-
-        var count: String = "0"
+        countToTokenizer = ""
+        count = "0"
         binding.count.text = count
         binding.scrollCount.viewTreeObserver.addOnGlobalLayoutListener {
             binding.scrollCount.post {
@@ -56,7 +63,7 @@ class MainActivity : AppCompatActivity() {
                     binding.count.text = count
                 } else if (count.length <= 14 && !count.contains(".")) { // Ограничение длины
                     count += number
-                    binding.count.text = spaceController.start(count, count.length)
+                    binding.count.text = spaceController.start(count)
                     textSizeFormating(count)
                 } else if (count.length <= 25 && count.contains(".")) {
                     count += number
@@ -66,23 +73,140 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        fun cleanCount() {
-            count = "0"
-            binding.count.text = count
-            textSizeFormating(count)
+        binding.one.setOnClickListener() {
+            appendNumber("1")
+        }
+        binding.two.setOnClickListener() {
+            appendNumber("2")
+        }
+        binding.three.setOnClickListener() {
+            appendNumber("3")
+        }
+        binding.four.setOnClickListener() {
+            appendNumber("4")
+        }
+        binding.five.setOnClickListener() {
+            appendNumber("5")
+        }
+        binding.six.setOnClickListener() {
+            appendNumber("6")
+        }
+        binding.seven.setOnClickListener() {
+            appendNumber("7")
+        }
+        binding.eight.setOnClickListener() {
+            appendNumber("8")
+        }
+        binding.nine.setOnClickListener() {
+            appendNumber("9")
+        }
+        binding.zero.setOnClickListener() {
+            appendNumber("0")
+        }
+        binding.point.setOnClickListener() {
+            appendNumber(".")
         }
 
-        binding.one.setOnClickListener() { appendNumber("1") }
-        binding.two.setOnClickListener() { appendNumber("2") }
-        binding.three.setOnClickListener() { appendNumber("3") }
-        binding.four.setOnClickListener() { appendNumber("4") }
-        binding.five.setOnClickListener() { appendNumber("5") }
-        binding.six.setOnClickListener() { appendNumber("6") }
-        binding.seven.setOnClickListener() { appendNumber("7") }
-        binding.eight.setOnClickListener() { appendNumber("8") }
-        binding.nine.setOnClickListener() { appendNumber("9") }
-        binding.zero.setOnClickListener() { appendNumber("0") }
-        binding.point.setOnClickListener() { appendNumber(".") }
-        binding.ac.setOnClickListener() { cleanCount() }
+        binding.ac.setOnClickListener() {
+            val runnable = Runnable {
+                exceptionButton(binding.ac)
+            }
+            handler.post { runnable.run() }
+        }
+        binding.percent.setOnClickListener() {
+            countToTokenizer = "$count%"
+            val runnable = Runnable {
+                exceptionButton(binding.percent)
+            }
+            handler.post { runnable.run() }
+        }
+        binding.divide.setOnClickListener(){
+
+        }
+        binding.multiply.setOnClickListener(){
+
+        }
+        binding.minus.setOnClickListener(){
+
+        }
+        binding.plus.setOnClickListener(){
+
+        }
+        binding.equal.setOnClickListener(){
+
+        }
+    }
+
+    private fun cleanCount() {
+        countToTokenizer = DEF_COUNT_TOKENIZER
+        count = DEF_COUNT
+        binding.count.text = count
+        textSizeFormating(count)
+    }
+
+    private fun textSizeFormating(count: String) {
+        if (count.length >= 7) {
+            binding.count.textSize = 65.0F
+        } else binding.count.textSize = 90.0F
+    }
+
+    private fun exceptionButton(view: View) {
+        val listActions = mutableListOf(
+            binding.percent,
+            binding.divide,
+            binding.multiply,
+            binding.minus,
+            binding.plus,
+            binding.equal
+        )
+
+        if (listActions.contains(view)) {
+            listActions.remove(view)
+        }
+
+        when (view) {
+            binding.ac -> {
+                listActions.forEach { view ->
+                    view.alpha = 1F
+                }
+                cleanCount()
+            }
+
+            binding.plusOrMinus -> {
+            }
+
+            binding.percent -> {
+                listActions.forEach { view ->
+                    view.alpha = 0.4F
+                }
+            }
+
+            binding.divide -> {
+                listActions.forEach { view ->
+                    view.alpha = 0.4F
+                }
+            }
+            binding.multiply -> {
+                listActions.forEach { view ->
+                    view.alpha = 0.4F
+                }
+            }
+            binding.minus -> {
+                listActions.forEach { view ->
+                    view.alpha = 0.4F
+                }
+            }
+            binding.plus -> {
+                listActions.forEach { view ->
+                    view.alpha = 0.4F
+                }
+            }
+            binding.equal -> {}
+        }
+    }
+
+    companion object {
+        private const val DEF_COUNT = "0"
+        private const val DEF_COUNT_TOKENIZER = "0"
     }
 }
--- app/src/main/java/com/example/calculator/SpaceController.kt
+++ app/src/main/java/com/example/calculator/SpaceController.kt
@@ -1,115 +1,15 @@
 package com.example.calculator
 
 class SpaceController {
-    fun start(count: String, length: Int): String {
-        if (!count.contains(".")) {
-            val trimCount = count.replace(" ", "")
-            when (length) {
-                4 -> {
-                    val index = 1
-                    val reformatCount = StringBuilder(trimCount)
-                    reformatCount.insert(index, " ")
-                    return reformatCount.toString()
-                }
-
-                5 -> {
-                    val index = 2
-                    val reformatCount = StringBuilder(trimCount)
-                    reformatCount.insert(index, " ")
-                    return reformatCount.toString()
-                }
-
-                6 -> {
-                    val index = 3
-                    val reformatCount = StringBuilder(trimCount)
-                    reformatCount.insert(index, " ")
-                    return reformatCount.toString()
-                }
-
-                7 -> {
-                    val indices = listOf(1, 4)
-                    val reformatCount = StringBuilder(count)
-                    for (index in indices.sortedDescending()) {
-                        reformatCount.insert(index, " ")
-                    }
-                    return reformatCount.toString()
-                }
-
-                8 -> {
-                    val indices = listOf(2, 5)
-                    val reformatCount = StringBuilder(count)
-                    for (index in indices.sortedDescending()) {
-                        reformatCount.insert(index, " ")
-                    }
-                    return reformatCount.toString()
-                }
-
-                9 -> {
-                    val indices = listOf(3, 6)
-                    val reformatCount = StringBuilder(count)
-                    for (index in indices.sortedDescending()) {
-                        reformatCount.insert(index, " ")
-                    }
-                    return reformatCount.toString()
-                }
-
-                10 -> {
-                    val indices = listOf(1, 4, 7)
-                    val reformatCount = StringBuilder(count)
-                    for (index in indices.sortedDescending()) {
-                        reformatCount.insert(index, " ")
-                    }
-                    return reformatCount.toString()
-                }
-
-                11 -> {
-                    val indices = listOf(2, 5, 8)
-                    val reformatCount = StringBuilder(count)
-                    for (index in indices.sortedDescending()) {
-                        reformatCount.insert(index, " ")
-                    }
-                    return reformatCount.toString()
-                }
-
-                12 -> {
-                    val indices = listOf(3, 6, 9)
-                    val reformatCount = StringBuilder(count)
-                    for (index in indices.sortedDescending()) {
-                        reformatCount.insert(index, " ")
-                    }
-                    return reformatCount.toString()
-                }
-
-                13 -> {
-                    val indices = listOf(1, 4, 7, 10)
-                    val reformatCount = StringBuilder(count)
-                    for (index in indices.sortedDescending()) {
-                        reformatCount.insert(index, " ")
-                    }
-                    return reformatCount.toString()
-                }
-
-                14 -> {
-                    val indices = listOf(2, 5, 8, 11)
-                    val reformatCount = StringBuilder(count)
-                    for (index in indices.sortedDescending()) {
-                        reformatCount.insert(index, " ")
-                    }
-                    return reformatCount.toString()
-                }
-
-                15 -> {
-                    val indices = listOf(3, 6, 9, 12)
-                    val reformatCount = StringBuilder(count)
-                    for (index in indices.sortedDescending()) {
-                        reformatCount.insert(index, " ")
-                    }
-                    return reformatCount.toString()
-                }
-
-                else -> return count
+    fun start(number: String): String {
+        val stringBuilder = StringBuilder()
+        val length = number.length
+        for (i in 0 until length) {
+            if ((length - i) % 3 == 0 && i != 0) {
+                stringBuilder.append(' ')
             }
+            stringBuilder.append(number[i])
         }
-        return count
+        return stringBuilder.toString()
     }
 }
